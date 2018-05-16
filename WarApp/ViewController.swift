fc3cf66d3307fecd09bae600ad0e8a1d3c590506
Marco Jalili <smhj63@gmail.com>
2018-05-16 14:49:42 +1000

    first commit

--- WarApp/ViewController.swift
+++ WarApp/ViewController.swift
@@ -9,17 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var leftImageView: UIImageView!
 
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //leftImageView.image
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        // Randomize the image numbers
+        let leftRandomNumber = arc4random_uniform(13) + 2
+        let rightRandomNumber = arc4random_uniform(13) + 2
+        
+        // Change the image views
+        leftImageView.image = UIImage (named: "card\(leftRandomNumber)")
+        rightImageView.image = UIImage (named: "card\(rightRandomNumber)")
+        
+        // Compare the numbers
+        if leftRandomNumber > rightRandomNumber {
+            
+            leftScore += 1                               //update the score
+            leftScoreLabel.text = String(leftScore)      //update the label
+        }
+        else if rightRandomNumber > leftRandomNumber {
+            
+            rightScore += 1                              //update the score
+            rightScoreLabel.text = String(rightScore)    //update the label
+        }
+        else if leftRandomNumber == rightRandomNumber {
+            
+        }
+    }
+    
 }
 
